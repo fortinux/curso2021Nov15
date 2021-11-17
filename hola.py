e898a99f16d254f940655ef74d1924f7957f2ae6
fortinux <info@fortinux.com>
2021-11-17 10:16:26 +0100

    sincronizando repositorios

---
+++ hola.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+[Add module documentation here]
+
+Author: Fortinux
+info@fortinux.com
+Date: [Add date here]
+"""
+print("Hola mundo!")
+for i in numbers:
+    print(i)
+
+
